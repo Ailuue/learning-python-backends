41791fbe289f4b6a09f69289b272645324ecf108
Alex Vervloet <ewokpanda@gmail.com>
2026-05-06 16:40:00 +0800

    feat: add bst operations

--- d-structs-algos/searching/binary_search_tree/binary_search_tree.py
+++ d-structs-algos/searching/binary_search_tree/binary_search_tree.py
@@ -18,6 +18,45 @@ class binary_search_tree_node:
                 self.right = binary_search_tree_node(val)
             else:
                 self.right.insert(val)
+                
+    def delete(self, val):
+        if self.val is None:
+            return self
+        if val < self.val:
+            if self.left is not None:
+                self.left = self.left.delete(val)
+        elif val > self.val:
+            if self.right is not None:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+            min_larger_node = self.right
+            while min_larger_node.left is not None:
+                min_larger_node = min_larger_node.left
+            self.val = min_larger_node.val
+            self.right = self.right.delete(min_larger_node.val)
+        return self
+    
+    def preorder(self, visited):
+        if self.val:
+            visited.append(self.val)
+        if self.left:
+            self.left.preorder(visited)
+        if self.right:
+            self.right.preorder(visited)
+        return visited
+    
+    def postorder(self, visited):
+        if self.left:
+            self.left.postorder(visited)
+        if self.right:
+            self.right.postorder(visited)
+        if self.val:
+            visited.append(self.val)
+        return visited
 
     def inorder(self, result=None):
         if result is None:
@@ -29,3 +68,25 @@ class binary_search_tree_node:
         if self.right:
             self.right.inorder(result)
         return result
+    
+    def exists(self, val):
+        if self.val == val:
+            return True
+        if val < self.val:
+            if self.left:
+                return self.left.exists(val)
+        if val > self.val:
+            if self.right:
+                return self.right.exists(val)
+        return False
+    
+    def height(self):
+        left = 0
+        right = 0
+        if not self.val:
+            return 0
+        if self.left:
+             left = self.left.height()
+        if self.right:
+            right = self.right.height()
+        return max(left, right) + 1
