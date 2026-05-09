5a8d972f0ff5071ec0ec5b750c3d4b4c694eb884
Alex Vervloet <ewokpanda@gmail.com>
2026-05-09 12:24:53 +0800

    fix: allow cors

--- backends/tutorial/main.py
+++ backends/tutorial/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from enum import Enum
 
 class Messages(str, Enum):
@@ -6,6 +7,17 @@ class Messages(str, Enum):
     goodbye = "goodbye"
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!"}
 
 @app.get("/cool")
 async def cool_message():
