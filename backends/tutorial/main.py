0e81568dd2def08f5c58a4e064968e601675c96d
Alex Vervloet <ewokpanda@gmail.com>
2026-05-11 09:26:13 +0800

    fix: overflow issues with response cards

--- backends/tutorial/main.py
+++ backends/tutorial/main.py
@@ -2,27 +2,45 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from enum import Enum
 
-class Messages(str, Enum):
-    hello = "hello"
-    goodbye = "goodbye"
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
 
-@app.get("/cool")
-async def cool_message():
-    return {"message": "You are cool!"}
 
-@app.get("/{message}")
-async def echo_message(message: Messages):
-    return {"message": message}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def echo_message(model_name: ModelName):
+    return {"model_name": model_name}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
