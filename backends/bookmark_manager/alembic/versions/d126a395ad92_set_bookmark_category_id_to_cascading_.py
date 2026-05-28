739820218b453965dc7761f470e098ed027306c2
Alex Vervloet <ewokpanda@gmail.com>
2026-05-28 15:47:47 +0800

    refactor: remove unused sqlalchemy and sqlmodel imports from cascading FK migration

--- backends/bookmark_manager/alembic/versions/d126a395ad92_set_bookmark_category_id_to_cascading_.py
+++ backends/bookmark_manager/alembic/versions/d126a395ad92_set_bookmark_category_id_to_cascading_.py
@@ -9,8 +9,6 @@ Create Date: 2026-05-19 10:03:24.830237
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-import sqlmodel
 
 
 # revision identifiers, used by Alembic.
